ecee10e3c0080a8bb4a18eabfb0a2e7c500eb9fa
T Do <tdo02@jaguar.tamu.edu>
2025-11-07 23:38:26 -0600

    replace ktinker with customtkinter

--- frontend.py
+++ frontend.py
@@ -1,165 +1,159 @@
-import tkinter as tk
-import tkinter.font as tkFont
+# pip install customtkinter
+import customtkinter as ctk
+from PIL import Image
 
-
-# Set up window size, position
-window = tk.Tk()
-window.title("Electricity Database Management")
+# set up window size, position
 window_width = 700
 window_height = 400
+window = ctk.CTk()
+window.title("Electricity Database Management")
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 x = int((screen_width / 2) - (window_width / 2))
 y = int((screen_height / 2) - (window_height / 2))
 window.geometry(f'{window_width}x{window_height}+{x}+{y}')
-
-# Window icon
+window.resizable(False, False)
 window.iconbitmap("icon.ico")
 
-# Set backround image
-bg_img = tk.PhotoImage(file="background.png")
-bg_label = tk.Label(window, image=bg_img)
-bg_label.place(x=0, y=0, relwidth=1, relheight=1)
 
-# Font size
-font_large = tkFont.Font(family="Helvetica", size=16, weight="bold")
+# background image
+bg_image = ctk.CTkImage(dark_image=Image.open("background.png"), size=(700, 400))
+bg_label = ctk.CTkLabel(window, image=bg_image, text="")
+bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+ctk.set_appearance_mode("dark")
+window.configure(fg_color="#000000")
+
 
 def hide_all_buttons():
     add_button.place_forget()
     edit_button.place_forget()
     delete_button.place_forget()
     view_button.place_forget()
-    back_button.place(x=650, y=20)
+    back_button.place(x=600, y=15)
 
-def show_add_screen():
-    hide_all_buttons()
-    
-    # Add query
-    label = tk.Label(window, text="Country Code", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=80)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=5, font=("Helvetica", 9))
-    entry.place(x=350, y=80)
-
-    label = tk.Label(window, text="Country Name", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=120)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=20, font=("Helvetica", 9))
-    entry.place(x=350, y=120)
-
-    label = tk.Label(window, text="Number of people", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=160)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=12, font=("Helvetica", 9))
-    entry.place(x=350, y=160)
-
-    label = tk.Label(window, text="Year", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=200)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=6, font=("Helvetica", 9))
-    entry.place(x=350, y=200)
-
-    # Execute button
-    confirm_button = tk.Button(window, text="ADD", font=("Helvetica", 14, "bold"), width=8, bg="#588157", fg="white", cursor='hand2')
-    confirm_button.place(x=300, y=300)
-
-def show_edit_screen():
-    hide_all_buttons()
-
-    label = tk.Label(window, text="Country Code", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=80)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=5, font=("Helvetica", 9))
-    entry.place(x=350, y=80)
-
-    label = tk.Label(window, text="Country Name", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=120)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=20, font=("Helvetica", 9))
-    entry.place(x=350, y=120)
-
-    label = tk.Label(window, text="Number of people", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=160)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=12, font=("Helvetica", 9))
-    entry.place(x=350, y=160)
-
-    label = tk.Label(window, text="Year", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=200)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=6, font=("Helvetica", 9))
-    entry.place(x=350, y=200)
-
-    save_button = tk.Button(window, text="SAVE", font=("Helvetica", 14, "bold"), bg="#0077b6", fg="white", cursor='hand2')
-    save_button.place(x=300, y=300)
-
-def show_delete_screen():
-    hide_all_buttons()
-
-    label = tk.Label(window, text="Country Code", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=120)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=5, font=("Helvetica", 9))
-    entry.place(x=350, y=120)
-
-    label = tk.Label(window, text="Country Name", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=160)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=20, font=("Helvetica", 9))
-    entry.place(x=350, y=160)
-
-    label = tk.Label(window, text="Year", font=("Helvetica", 9, "bold"))
-    label.place(x=235, y=200)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=6, font=("Helvetica", 9))
-    entry.place(x=350, y=200)
-
-    del_button = tk.Button(window, text="DELETE", font=("Helvetica", 14, "bold"), bg="#c1121f", fg="white", cursor='hand2')
-    del_button.place(x=300, y=300)
-
-def show_view_screen():
-    hide_all_buttons()
-
-    label = tk.Label(window, text="Country Code", font=("Helvetica", 9, "bold"))
-    label.place(x=215, y=120)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=5, font=("Helvetica", 9))
-    entry.place(x=310, y=120)
-
-    label = tk.Label(window, text="OR", font=("Helvetica", 9, "bold"))
-    label.place(x=315, y=160)
-
-    label = tk.Label(window, text="Country Name", font=("Helvetica", 9, "bold"))
-    label.place(x=215, y=200)
-    entry_var = tk.StringVar()
-    entry = tk.Entry(window, textvariable=entry_var, width=20, font=("Helvetica", 9))
-    entry.place(x=310, y=200)    
-
-
-    search_button = tk.Button(window, text="SEARCH", font=("Helvetica", 14, "bold"), cursor='hand2')
-    search_button.place(x=300, y=300)
-
-
-def back_button_event():
-    for i in window.winfo_children():
-        if i is bg_label:
+# clear everything in the window except background
+def clear_screen(exceptions=()):
+    for w in window.winfo_children():
+        if w in exceptions:
             continue
-        i.place_forget()
+        w.place_forget()
+
+# defind button event
+def back_button_event():
+    clear_screen(exceptions=[bg_label])
     add_button.place(x=150, y=100)
     edit_button.place(x=400, y=100)
     delete_button.place(x=150, y=190)
     view_button.place(x=400, y=190)
-    
-#  Define buttons
-add_button = tk.Button(window, text="ADD", width=10, height=2, font=font_large, command=show_add_screen, bg="#588157", cursor='hand2')
+    back_button.place_forget()
+
+def show_add_screen():
+    hide_all_buttons()
+
+    country_c = ctk.CTkLabel(window, text="Country Code", font=("Helvetica", 12, "bold"))
+    country_c.place(x=235, y=80)
+    country_c2 = ctk.CTkEntry(window, width=60, fg_color= '#e5e5e5')
+    country_c2.place(x=350, y=80)
+
+    country_n = ctk.CTkLabel(window, text="Country Name", font=("Helvetica", 12, "bold"))
+    country_n.place(x=235, y=120)
+    country_n2 = ctk.CTkEntry(window, width=180, fg_color= '#e5e5e5')
+    country_n2.place(x=350, y=120)
+
+    n_people = ctk.CTkLabel(window, text="Number of People", font=("Helvetica", 12, "bold"))
+    n_people.place(x=235, y=160)
+    n_people2 = ctk.CTkEntry(window, width=120, fg_color= '#e5e5e5')
+    n_people2.place(x=350, y=160)
+
+    year = ctk.CTkLabel(window, text="Year", font=("Helvetica", 12, "bold"))
+    year.place(x=235, y=200)
+    year2 = ctk.CTkEntry(window, width=80, fg_color= '#e5e5e5')
+    year2.place(x=350, y=200)
+
+    add_confirm = ctk.CTkButton(window, text="ADD", width=120, height=40, fg_color="#588157", hover_color="#436644", cursor="hand2")
+    add_confirm.place(x=300, y=300)
+
+def show_edit_screen():
+    hide_all_buttons()
+
+    country_c = ctk.CTkLabel(window, text="Country Code", font=("Helvetica", 12, "bold"))
+    country_c.place(x=235, y=80)
+    country_c2 = ctk.CTkEntry(window, width=60, fg_color= '#e5e5e5')
+    country_c2.place(x=350, y=80)
+
+    country_n = ctk.CTkLabel(window, text="Country Name", font=("Helvetica", 12, "bold"))
+    country_n.place(x=235, y=120)
+    country_n2 = ctk.CTkEntry(window, width=180, fg_color= '#e5e5e5')
+    country_n2.place(x=350, y=120)
+
+    n_people = ctk.CTkLabel(window, text="Number of People", font=("Helvetica", 12, "bold"))
+    n_people.place(x=235, y=160)
+    n_people2 = ctk.CTkEntry(window, width=120, fg_color= '#e5e5e5')
+    n_people2.place(x=350, y=160)
+
+    year = ctk.CTkLabel(window, text="Year", font=("Helvetica", 12, "bold"))
+    year.place(x=235, y=200)
+    year2 = ctk.CTkEntry(window, width=80, fg_color= '#e5e5e5')
+    year2.place(x=350, y=200)
+
+    save_btn = ctk.CTkButton(window, text="SAVE", width=120, height=40, fg_color="#0077b6", hover_color="#025b87", cursor="hand2")
+    save_btn.place(x=300, y=300)
+
+def show_delete_screen():
+    hide_all_buttons()
+
+    country_c = ctk.CTkLabel(window, text="Country Code", font=("Helvetica", 12, "bold"))
+    country_c.place(x=235, y=120)
+    country_c2 = ctk.CTkEntry(window, width=60, fg_color= '#e5e5e5')
+    country_c2.place(x=350, y=120)
+
+    country_n = ctk.CTkLabel(window, text="Country Name", font=("Helvetica", 12, "bold"))
+    country_n.place(x=235, y=160)
+    country_n2 = ctk.CTkEntry(window, width=180, fg_color= '#e5e5e5')
+    country_n2.place(x=350, y=160)
+
+    year = ctk.CTkLabel(window, text="Year", font=("Helvetica", 12, "bold"))
+    year.place(x=235, y=200)
+    year2 = ctk.CTkEntry(window, width=80, fg_color= '#e5e5e5')
+    year2.place(x=350, y=200)
+
+    del_btn = ctk.CTkButton(window, text="DELETE", width=120, height=40, fg_color="#c1121f", hover_color="#960d17", cursor="hand2")
+    del_btn.place(x=300, y=300)
+
+
+def show_view_screen():
+    hide_all_buttons()
+
+    country_c = ctk.CTkLabel(window, text="Country Code", font=("Helvetica", 12, "bold"))
+    country_c.place(x=215, y=120)
+    country_c2 = ctk.CTkEntry(window, width=60, fg_color= '#e5e5e5')
+    country_c2.place(x=320, y=120)
+
+    label = ctk.CTkLabel(window, text="OR", font=("Helvetica", 12, "bold"))
+    label.place(x=330, y=160)
+
+    country_n = ctk.CTkLabel(window, text="Country Name", font=("Helvetica", 12, "bold"))
+    country_n.place(x=215, y=200)
+    country_n2 = ctk.CTkEntry(window, width=180, fg_color= '#e5e5e5')
+    country_n2.place(x=320, y=200)
+
+    search_btn = ctk.CTkButton(window, text="SEARCH", width=120, height=40, fg_color="#4a4e69", hover_color="#34344d", cursor="hand2")
+    search_btn.place(x=300, y=300)
+
+
+# define buttons
+add_button = ctk.CTkButton(window, text="ADD", width=140, height=50, font=("Helvetica", 16, "bold"), fg_color="#588157",hover_color="#436644", cursor="hand2", command=show_add_screen)
 add_button.place(x=150, y=100)
-add_content = False
-edit_button = tk.Button(window, text="EDIT", command=show_edit_screen, width=10, height=2, font=font_large, bg="#ffd60a", cursor='hand2')
+
+edit_button = ctk.CTkButton(window, text="EDIT", width=140, height=50, font=("Helvetica", 16, "bold"), fg_color="#fca311", hover_color="#c2a800", cursor="hand2", command=show_edit_screen)
 edit_button.place(x=400, y=100)
-delete_button = tk.Button(window, text="DELETE", command=show_delete_screen, width=10, height=2, font=font_large, bg="#c1121f", cursor='hand2')
+
+delete_button = ctk.CTkButton(window, text="DELETE", width=140, height=50, font=("Helvetica", 16, "bold"), fg_color="#c1121f", hover_color="#960d17", cursor="hand2", command=show_delete_screen)
 delete_button.place(x=150, y=190)
-view_button = tk.Button(window, text="VIEW", command=show_view_screen, width=10, height=2, font=font_large, cursor='hand2')
+
+view_button = ctk.CTkButton(window, text="VIEW", width=140, height=50, font=("Helvetica", 16, "bold"), fg_color="#4a4e69", hover_color="#34344d", cursor="hand2", command=show_view_screen)
 view_button.place(x=400, y=190)
-back_button = tk.Button(window, text="BACK", command=back_button_event, height=1, cursor='hand2')
+
+back_button = ctk.CTkButton(window, text="BACK", width=80, height=30, fg_color="#adb5bd", hover_color="#6c757d", cursor="hand2", command=back_button_event)
 
 window.mainloop()
